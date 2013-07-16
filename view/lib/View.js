4cfa9bc2a04eff6337a3f21da7ba3aa55c73b309
Michael Williams <dinosaur@riseup.net>
2013-07-15 23:26:37 -0700

    [view] left out of 893aeb0

--- view/lib/View.js
+++ view/lib/View.js
@@ -61,55 +61,80 @@ var View = function (options) {
 // async loads a View
 //
 View.prototype.load = function (callback) {
-  var self = this;
+  var self = this,
+      async = require('async');
 
-  // TODO make these async?
   // TODO error handling
 
-  self.loadTemplate(self);
-  self.loadPresenter(self);
-
-  if (self.viewPath) {
-    return self.loadViewPath(callback);
-  } else {
-    return callback(null, self);
-  }
+  async.parallel([
+    function(callback) {
+      return self.loadTemplate(self, callback);
+    },
+    function(callback) {
+      return self.loadPresenter(self, callback);
+    },
+    function(callback) {
+      if (self.viewPath) {
+        return self.loadViewPath(callback);
+      } else {
+        return callback(null, self);
+      }
+    }],
+    function(err) {
+      return callback(err, self);
+    });
 };
 
 //
-// sync loads a View template
+// async loads a View template
 //
-View.prototype.loadTemplate = function (options) {
+View.prototype.loadTemplate = function (options, callback) {
 
-  var self = this;
+  var self = this,
+      async = require('async');
 
-  if (options.template) {
-    // use given template
-    self.template = options.template;
-  } else if (options.templatePath) {
-    // load template from file
-    // TODO: error handling
-    self.template = fs.readFileSync(options.templatePath);
+  async.series([
+    // setup self.template
+    function(callback) {
+      if (options.template) {
+        // use given template
+        self.template = options.template;
+      } else if (options.templatePath) {
+        // load template from file
+        // TODO: error handling
+        self.template = fs.readFileSync(options.templatePath).toString();
 
-    // based on the template file extension,
-    var templateLang = path.extname(options.templatePath);
-    // use the corresponding template resource, if exists
-    if (resource.resources[templateLang]) {
-      // save the render fn in the View
-      self.render = resource.use(templateLang).render;
-    }
-  }
-
-  // update the querySelector context
-  self.$ = self.querySelector = query(self.template);
-
-  return self;
+        // based on the template file extension,
+        var templateLang = path.extname(options.templatePath).replace(/^./, '');
+        // use the corresponding template resource, if exists
+        var templateRes = resource.resources["template-" + templateLang];
+        if (templateRes) {
+          // initialize the resource
+          return templateRes.init(function() {
+            // save the render fn in the View
+            self.templateRender = templateRes.render;
+            return callback(null);
+          });
+        }
+      }
+      return callback(null);
+    },
+    // load self.template into querySelector context
+    function(callback) {
+      // update the querySelector context
+      self.$ = self.querySelector = query(self.template);
+      return callback(null);
+    }],
+    // return self, and errors if any
+    function(err) {
+      return callback(err, self);
+    });
 };
 
 //
 // sync loads a View presenter
 //
-View.prototype.loadPresenter = function(options) {
+View.prototype.loadPresenter = function(options, callback) {
 
   var self = this;
 
@@ -122,7 +147,7 @@ View.prototype.loadPresenter = function(options) {
     self.presenter = require(options.presenterPath);
   }
 
-  return self;
+  return callback(null, self);
 };
 
 //
@@ -214,16 +239,16 @@ View.prototype.loadViewPath = function (callback) {
           subView.presenterPath = filePath;
           break;
 
-        case ".html": // template
-          subView.templatePath = filePath;
-          break;
-
         case ".css": // stylesheet
           // TODO
           break;
 
-        default:
-          logger.warn("unknown view file: ", filePath);
+        default: // template
+          if (subView.templatePath) {
+            return callback(new Error("multiple templates given for " + subView.breadcrumb()));
+          }
+          subView.templatePath = filePath;
+          break;
       }
 
       subView.load(function(err) {
@@ -276,15 +301,26 @@ View.prototype.getSubView = function(viewPath) {
 //
 View.prototype.present = function(options, callback) {
 
-  // freshly load template
-  if (this.template) {
-    this.loadTemplate(this.template);
-  }
+  var self = this,
+      async = require('async');
 
-  // if we have presenter, use it,
-  // otherwise fallback to template render,
-  // otherwise fallback to default render.
-  return (this.presenter || this.render || render).call(this, options, callback);
+  // setup
+  async.series([
+    // if self has template, load it fresh
+    function (callback) {
+      if (self.template) {
+        return self.loadTemplate(self.template, callback);
+      }
+      return callback(null);
+    }],
+    function (err) {
+      if (err) { return callback(err); }
+
+      // if we have presenter, use it,
+      // otherwise fallback to template render,
+      // otherwise fallback to default render.
+      return (self.presenter || self.render).call(self, options, callback);
+    });
 };
 
 View.prototype.breadcrumb = function () {
@@ -294,8 +330,9 @@ View.prototype.breadcrumb = function () {
   return this.parent.breadcrumb() + '/' + this.name;
 };
 
-// export query and layout
+// export query, layout, and render
 View.prototype.query = query;
 View.prototype.layout = require('./layout');
+View.prototype.render = render;
 
 module['exports'] = View;
